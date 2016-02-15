16c1b20aed37b7fdd7cabe01d5f5bdd936813524
Vitor Gonçalves <vitorbg92@gmail.com>
2016-02-15 20:03:23 -0200

    Primeiro Commit

---
+++ Projeto Final/Assets/Scripts/Boss.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss : MonoBehaviour {
+
+    public bool andar;
+    public float movimentoX;
+    public Animator anime;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Projeto Final/Assets/Scripts/Orquicio.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orquicio : MonoBehaviour {
+	public bool walk;
+	public bool attack;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Horizontal") != 0) {
+			walk = true;
+		} else {
+			walk = false;
+		}
+
+		if (Input.GetButtonDown ("Fire1")) {
+			attack = true;
+		} else {
+			attack = false;
+		}
+
+		GetComponent<Animator> ().SetBool ("walk", walk);
+		GetComponent<Animator> ().SetBool ("attack", attack);
+	}
+}
